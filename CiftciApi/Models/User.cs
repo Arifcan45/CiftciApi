3656766b859653cd042060021d950c0324b12fac
Can Onay <arifonay.853@gmail.com>
2025-05-26 13:33:26 +0300

    Add project files.

---
+++ CiftciApi/Models/User.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CiftciApi.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(100)]
+        public string ?Name { get; set; }
+
+        [Required, StringLength(100), EmailAddress]
+        public string? Email { get; set; }
+
+        [Required, StringLength(20)]
+        public string ?PhoneNumber { get; set; }
+
+        [Required, StringLength(100)]
+        public string ?Password { get; set; }
+
+        [Required]
+        public UserType UserType { get; set; }
+
+        public double? Rating { get; set; }
+
+        public int? ReviewCount { get; set; }
+
+        public string? ProfileImageUrl { get; set; }
+
+        // Çiftçi için ek alanlar
+        public string? Province { get; set; } // İl
+        public string? District { get; set; } // İlçe
+        public string? Village { get; set; } // Köy
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+
+    public enum UserType
+    {
+        Alici = 1,
+        Ciftci = 2
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CiftciApi.Models
+{
+    public class User
+    {
+         [Key]
+        public int Id { get; set; }
+        
+        [Required, StringLength(100)]
+        public string Name { get; set; }
+        
+        [Required, StringLength(100), EmailAddress]
+        public string Email { get; set; }
+        
+        [Required, StringLength(20)]
+        public string PhoneNumber { get; set; }
+        
+        [Required, StringLength(100)]
+        public string Password { get; set; }
+        
+        [Required]
+        public UserType UserType { get; set; }
+        
+        public double? Rating { get; set; }
+        
+        public int? ReviewCount { get; set; }
+        
+        public string? ProfileImageUrl { get; set; }
+        
+        // Çiftçi için ek alanlar
+        public string? Province { get; set; } // İl
+        public string? District { get; set; } // İlçe
+        public string? Village { get; set; } // Köy
+        
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        
+    }
+}
